faf92cd0ab9dc7dd101823ea161052c0f7616036
greent-xl <113757146+greent-xl@users.noreply.github.com>
2023-04-30 01:34:47 +0300

    27

--- Mathematic/Mathematic/Math.cpp
+++ Mathematic/Mathematic/Math.cpp
@@ -2,12 +2,16 @@
 #include <vector>
 #include<queue>
 #include<stack>
+#include<string>
+#include<random>
+#include<time.h>
+#include<iomanip>
 using namespace std;
 
-int fun(int var, int a, int b) {
-    return (var * (a * a + b * b) + a * a + b * b + a + b) % 10;
+int fun() {
+    return rand() % 100 + 1;
 }
-void task2(int Var) {
+void task2() {
     const int SIZE = 9;
     int a[SIZE][SIZE]; // матрица связей
     int d[SIZE]; // минимальное расстояние
@@ -22,119 +26,118 @@ void task2(int Var) {
 
             if ((i + 1) == 1) {
                 if ((j + 1) == 2 or (j + 1) == 8 or (j + 1) == 9) {
-                    temp = fun(Var, i + 1, j + 1);
+                    temp = fun();
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
                 else if((j + 1) == 7){
-                    a[i][j] = fun(Var, i + 1, j + 1);
-                    a[j][i] = 1000;
+                    a[i][j] = fun();
+                    a[j][i] = 0;
                 }
                 else {
-                    temp = 1000;
+                    temp = 0;
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
             }
             else if ((i + 1) == 2) {
                 if ((j + 1) == 3 or (j + 1) == 7 or (j + 1) == 9) {
-                    temp = fun(Var, i + 1, j + 1);
+                    temp = fun();
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
                 else {
-                    temp = 1000;
+                    temp = 0;
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
             }
             else if ((i + 1) == 3) {
                 if ((j + 1) == 4 or (j + 1) == 6 or (j + 1) == 9) {
-                    temp = fun(Var, i + 1, j + 1);
+                    temp = fun();
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
                 else {
-                    temp = 1000;
+                    temp = 0;
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
             }
             else if ((i + 1) == 4) {
                 if ((j + 1) == 7) {
-                    temp = fun(Var, i + 1, j + 1);
+                    temp = fun();
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }else if ((j + 1) == 5 or (j + 1) == 6) {
-                    a[i][j] = fun(Var, i + 1, j + 1);
-                    a[j][i] = 1000;
+                    a[i][j] = fun();
+                    a[j][i] = 0;
                 }
                 else {
-                    temp = 1000;
+                    temp = 0;
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
             }
             else if ((i + 1) == 5) {
                 if ((j + 1) == 6) {
-                    temp = fun(Var, i + 1, j + 1);
+                    temp = fun();
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
                 else {
-                    temp = 1000;
+                    temp = 0;
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
             }
             else if ((i + 1) == 6) {
                 if ((j + 1) == 7 or (j + 1) == 8 or (j + 1) == 9) {
-                    temp = fun(Var, i + 1, j + 1);
+                    temp = fun();
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
                 else {
-                    temp = 1000;
+                    temp = 0;
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
             }
             else if ((i + 1) == 7) {
                 if ((j + 1) == 9) {
-                    temp = fun(Var, i + 1, j + 1);
+                    temp = fun();
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
                 else {
-                    temp = 1000;
+                    temp = 0;
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
             }
             else {
                 if ((j + 1) == 9) {
-                    temp = fun(Var, i + 1, j + 1);
+                    temp = fun();
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
                 else {
-                    temp = 1000;
+                    temp = 0;
                     a[i][j] = temp;
                     a[j][i] = temp;
                 }
             }
         }
     }
-    // Вывод матрицы связей
 
-    for (int i = 0; i < SIZE; i++)
-    {
+    cout << "Найти кратчайший пути от вершины 1 до вершины 4\nМатрица смежностей:\n";
+    // Вывод матрицы связей
+    for (int i = 0; i < SIZE; i++){
         for (int j = 0; j < SIZE; j++) {
-            std::cout << a[i][j] << " ";
+            std::cout << setw(2) << a[i][j] << " ";
         } 
         std::cout << "\n";
     }
-
     //Инициализация вершин и расстояний
     for (int i = 0; i < SIZE; i++)
     {
@@ -173,216 +176,36 @@ void task2(int Var) {
             v[minindex] = 0;
         }
     } while (minindex < 10000);
-    cout << d[5];
-}
 
-void task3() {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> g(n); // список инцидентных ребер каждой вершины
-    for (int i = 0; i < m; i++)
+    // Восстановление пути
+    int ver[SIZE]; // массив посещенных вершин
+    int end = 3; // индекс конечной вершины = 5 - 1
+    ver[0] = end + 1; // начальный элемент - конечная вершина
+    int k = 1; // индекс предыдущей вершины
+    int weight = d[end]; // вес конечной вершины
+
+    while (end != begin_index) // пока не дошли до начальной вершины
     {
-        int u, v;
-        cin >> u >> v;
-        g[u - 1].push_back(v - 1);
-        g[v - 1].push_back(u - 1);
-    }
-
-    // Проверка нечетности
-    int count_odd = 0;
-    for (int i = 0; i < n; i++)
-        if (g[i].size() % 2 == 1)
-            count_odd++;
-
-    // Подсчет количества "больших" компонент
-    int count_big_comp = 0;
-    vector<bool> use(n, false); // хранит состояния "посещенности" для всех вершин
-
-    // Перебор всех вершин для нахождения всех компонент
-    for (int i = 0; i < n; i++)
-    {
-        if (use[i]) // Проверка вершины на посещенность в какой-то из прошлых компонент
-            continue;
-        if (g[i].size() == 0) // Проверка вершины на изолированность
-            // Вообще тут по логике должна быть строка use[i] = true, но ее можно опустить
-            continue;
-
-        count_big_comp++;
-
-        // bfs от i-той вершины
-        queue<int> q;
-        q.push(i);
-        use[i] = true;
-
-        while (!q.empty())
-        {
-            int v = q.front();
-            q.pop();
-
-            for (int to : g[v])
+        for (int i = 0; i < SIZE; i++) // просматриваем все вершины
+            if (a[i][end] != 0)   // если связь есть
             {
-                if (use[to])
-                    continue;
-                q.push(to);
-                use[to] = true;
+                int temp = weight - a[i][end]; // определяем вес пути из предыдущей вершины
+                if (temp == d[i]) // если вес совпал с рассчитанным
+                {                 // значит из этой вершины и был переход
+                    weight = temp; // сохраняем новый вес
+                    end = i;       // сохраняем предыдущую вершину
+                    ver[k] = i + 1; // и записываем ее в массив
+                    k++;
+                }
             }
-        }
     }
-
-    if (count_odd <= 2 && count_big_comp <= 1)
-        cout << "YES\n";
-    else
-        cout << "NO\n";
-}
-void task31() {
-    int n = 0;//должно быть int n;
-    vector < vector<int> > g(n, vector<int>(n));
-
-    vector<int> deg(n);
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            deg[i] += g[i][j];
-
-    int first = 0;
-    while (!deg[first])  ++first;
-
-    int v1 = -1, v2 = -1;
-    bool bad = false;
-    for (int i = 0; i < n; ++i)
-        if (deg[i] & 1)
-            if (v1 == -1)
-                v1 = i;
-            else if (v2 == -1)
-                v2 = i;
-            else
-                bad = true;
-
-    if (v1 != -1)
-        ++g[v1][v2], ++g[v2][v1];
-
-    stack<int> st;
-    st.push(first);
-    vector<int> res;
-    while (!st.empty())
-    {
-        int v = st.top();
-        int i;
-        for (i = 0; i < n; ++i)
-            if (g[v][i])
-                break;
-        if (i == n)
-        {
-            res.push_back(v);
-            st.pop();
-        }
-        else
-        {
-            --g[v][i];
-            --g[i][v];
-            st.push(i);
-        }
+    string st = "";
+    cout << "\nВывод кратчайшего пути\n";
+    for (int i = k - 1; i >= 0; i--) {
+        st += to_string(ver[i]);
     }
-
-    if (v1 != -1)
-        for (size_t i = 0; i + 1 < res.size(); ++i)
-            if (res[i] == v1 && res[i + 1] == v2 || res[i] == v2 && res[i + 1] == v1)
-            {
-                vector<int> res2;
-                for (size_t j = i + 1; j < res.size(); ++j)
-                    res2.push_back(res[j]);
-                for (size_t j = 1; j <= i; ++j)
-                    res2.push_back(res[j]);
-                res = res2;
-                break;
-            }
-
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            if (g[i][j])
-                bad = true;
-
-    if (bad)
-        puts("-1");
-    else
-        for (size_t i = 0; i < res.size(); ++i)
-            printf("%d ", res[i] + 1);
+    cout << st;
 }
-//void findpath(int graph[][5], int n)
-//{
-//    vector<int> numofadj;
-//
-//    // Find out number of edges each vertex has
-//    for (int i = 0; i < n; i++)
-//        numofadj.push_back(accumulate(graph[i],
-//            graph[i] + 5, 0));
-//
-//    // Find out how many vertex has odd number edges
-//    int startpoint = 0, numofodd = 0;
-//    for (int i = n - 1; i >= 0; i--)
-//    {
-//        if (numofadj[i] % 2 == 1)
-//        {
-//            numofodd++;
-//            startpoint = i;
-//        }
-//    }
-//
-//    // If number of vertex with odd number of edges
-//    // is greater than two return "No Solution".
-//    if (numofodd > 2)
-//    {
-//        cout << "No Solution" << endl;
-//        return;
-//    }
-//
-//    // If there is a path find the path
-//    // Initialize empty stack and path
-//    // take the starting current as discussed
-//    stack<int> stack;
-//    vector<int> path;
-//    int cur = startpoint;
-//
-//    // Loop will run until there is element in the stack
-//    // or current edge has some neighbour.
-//    while (!stack.empty() or
-//        accumulate(graph[cur],
-//            graph[cur] + 5, 0) != 0)
-//    {
-//        // If current node has not any neighbour
-//        // add it to path and pop stack
-//        // set new current to the popped element
-//        if (accumulate(graph[cur],
-//            graph[cur] + 5, 0) == 0)
-//        {
-//            path.push_back(cur);
-//            cur = stack.top();
-//            stack.pop();
-//        }
-//
-//        // If the current vertex has at least one
-//        // neighbour add the current vertex to stack,
-//        // remove the edge between them and set the
-//        // current to its neighbour.
-//        else
-//        {
-//            for (int i = 0; i < n; i++)
-//            {
-//                if (graph[cur][i] == 1)
-//                {
-//                    stack.push(cur);
-//                    graph[cur][i] = 0;
-//                    graph[i][cur] = 0;
-//                    cur = i;
-//                    break;
-//                }
-//            }
-//        }
-//    }
-//
-//    // print the path
-//    for (auto ele : path) cout << ele << " -> ";
-//    cout << cur << endl;
-//}
 
 bool try_kuhn(int v, vector<char>& used, vector<int>& mt, vector<vector<int>>& g) {
     if (used[v])  return false;
@@ -397,16 +220,46 @@ bool try_kuhn(int v, vector<char>& used, vector<int>& mt, vector<vector<int>>& g
     return false;
 }
 void task7() {
-    int n, k, m;
-    cin >> n >> k >> m;//колво вершин левой доли, правой, кол-во ребер
+    int n, k;
+    n = rand() % 10 + 1;
+    k = rand() % 10 + 1;
     vector < vector<int> > g(n);
     vector<int> mt;
     vector<char> used;
-    for (int i = 0; i < m; i++)
+    vector<int> nums;
+    cout << n << ' ' << k << '\n';
+    for (int i = 0; i < n; i++) {
+        int p = rand() % k+1;
+        for (int j = 0; j < k; j++) {
+            cout << '\n' << j << '\n';
+            g[i].push_back(0);
+        }
+
+        /*nums.clear();*/
+    }
+    /*cout << n << ' ' << k << '\n';
+    for (int i = 0; i < n; i++)
     {
-        int u, v;
-        cin >> u >> v;//номера свзанных вершин, ребра
-        g[u - 1].push_back(v - 1);
+        int p = rand() % k;
+        cout << p << "\n";
+        vector<int> nums{1,2,3,4,5,6,7,8,9,10};
+        for (int j = 0; j < p; k++) {
+            int l = rand() % nums.size() + 1;
+            cout << l << '\n';
+            int u = i, v = nums[l - 1];
+            cout << u << " " << v << '\n';
+            g[u].push_back(v - 1);
+
+            auto iter = nums.begin();
+            nums.erase(iter + l - 1);
+        }
+    }*/
+    cout << "\nМатрица смежностей:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++) {
+            cout << setw(2) << g[i][j];
+        }
+        cout << '\n';
     }
     mt.assign(k, -1);
     vector<char> used1(n);
@@ -433,7 +286,29 @@ void task7() {
 
 int main()
 {
+    srand(time(NULL));
     setlocale(LC_ALL, "ru");
     task7();
+    /*int n, k;
+    n = rand() % 10 + 1;
+    k = rand() % 10 + 1;
+    cout << n << " " << k << "\n";
+    vector < vector<int> > g(n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++) {
+            cout << i << " " << j << ' ';
+            g[i].push_back(j);
+        }
+        cout << "\n";
+    }*/
+
+    //cout << "\nМатрица смежностей:\n";
+    //for (int i = 0; i < n; i++) {
+    //    for (int j = 0; j < k; j++) {
+    //        cout << setw(2) << g[i][j];
+    //    }
+    //    cout << '\n';
+    //}
     return 0;
 }
